abee3715d6b5ee1874887683bf1609b4eb3c1192
Sukanya-Kothapally <58608694+Sukanya-Kothapally@users.noreply.github.com>
2021-02-26 15:54:18 -0800

    Delete Week8 directory

--- Week8/Integration.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-integrated_df= groubbycounties_census.join(groubbycounties_covid)
-
-integrated_df['Totalcases'] = integrated_df.apply(lambda index: (index['cases'] * 100000) / index['TotalPop'], axis=1)
-integrated_df['Totaldeaths'] = integrated_df.apply(lambda index: (index['deaths'] * 100000) / index['TotalPop'], axis=1)
-#integrated_df.head(5)
